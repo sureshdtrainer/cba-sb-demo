5f0fea598af01ded4d2f87539427a5b8031c7949
sureshdtrainer <suresh.d.trainer@gmail.com>
2024-12-09 15:20:03 +0530

    Unit testing and mocking in Spring Boot

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.HelloWeb;
+import com.example.demo.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +14,12 @@ public class HelloWorldController {
     @Autowired
     HelloWeb helloWeb;
 
+    @Autowired
+    GreetingService greetingService;
+
     @GetMapping("hello")
     public String sayHello(){
-        return "Hello World";
+        return greetingService.greet();
     }
 
     @GetMapping("helloweb")
@@ -23,11 +28,14 @@ public class HelloWorldController {
         helloWeb.setMessage("Hello Web!");
         return helloWeb;
     }
-
+    //Accessing and using PATH Variable
     @GetMapping("/helloweb/name/{name}/{message}")
     public String sayHelloWithObjectPath(@PathVariable String name, @PathVariable String message){
         return "Hello " + name + " " +message;
     }
-
-
+    //Accessing Query String /helloweb/agelink?age=25&gender=M
+    @GetMapping("/helloweb/agelink")
+    public  String sayHelloWithQueryString(@RequestParam String age, @RequestParam String gender){
+        return "Hello, your age is  " +age +" " +gender;
+    }
 }
--- src/test/java/com/example/demo/DemoApplicationWithExternalDepTests.java
+++ src/test/java/com/example/demo/DemoApplicationWithExternalDepTests.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
 import com.example.demo.controller.HelloWorldController;
-import static org.assertj.core.api.Assertions.*;
-
 import com.example.demo.service.GreetingService;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
@@ -12,45 +10,35 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SuppressWarnings("removal")
 @SpringBootTest
 @AutoConfigureMockMvc
-class DemoApplicationTests {
+class DemoApplicationWithExternalDepTests {
 	@Autowired
 	private HelloWorldController controller;
 
 	@Autowired
 	private MockMvc mockMvc;
-
-	@MockBean
-	private GreetingService greetingService;
-
-	@Test
-	void contextLoads() {
-	}
-
 	@Test
 	public void contextLoadsWithControllerSuccess(){
 		assertThat(controller).isNotNull();
-	}//unit testing by mocking dependencies and testing only controller(unit of work)
+	}
 
+	//unit testing with dependencies (integration testing )
 	@Test
-	public void sayHelloShouldReturnMockedMessage() throws Exception {
+	public void sayHelloShouldReturnDefaultMessage() throws Exception {
 		// call /hello and check the actual and expected message
-		//Mock the behaviour the dependency
-		Mockito.when(greetingService.greet()).thenReturn("Bye from Greeting Service");
-
 		this.mockMvc.perform(get("/hello"))
 				.andDo(print())
 				.andExpect(status().isOk())
-				.andExpect(content().string(CoreMatchers.containsString("Bye from")));
+				.andExpect(content().string(CoreMatchers.containsString("Hello from")));
 	}
-
 
 }
